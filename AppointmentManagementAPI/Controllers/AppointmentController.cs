467ef13507364a08b76630f3b68cf13a27b97938
mdtuhinislam <mdtuhinislam@gmail.com>
2024-12-28 00:27:41 +0600

    Appointment API modified

--- AppointmentManagementAPI/Controllers/AppointmentController.cs
+++ AppointmentManagementAPI/Controllers/AppointmentController.cs
@@ -2,6 +2,7 @@
 using AppoinmentManagementAPI.Domain.Entities;
 using AppoinmentManagementAPI.Infrastructure.Database;
 using AppointmentManagementAPI.Application.Interfaces.Services.Appointments;
+using AppointmentManagementAPI.Presentation.UtilityModels;
 using CoreApiResponse;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,12 +22,18 @@ namespace AppointmentManagementAPI.Presentation.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateAppointment([FromBody] Appointment appointment)
+        public async Task<IActionResult> CreateAppointment([FromBody] AppointmentDTO appointment)
         {
             if (!ModelState.IsValid)
                 return CustomResult("Invalid data!", HttpStatusCode.BadRequest);
-
-            var isAdded = await _service.Add(appointment);
+            var _appointment = new Appointment
+            {
+                PatientName = appointment.PatientName,
+                PatientContactInfo = appointment.PatientContactInfo,
+                AppointmentDateTime = appointment.AppointmentDateTime,
+                DoctorId = appointment.DoctorId
+            };
+            var isAdded = await _service.Add(_appointment);
             if (!isAdded)
                 return CustomResult("Data not added!", HttpStatusCode.InternalServerError);
             return CustomResult("Created", appointment, HttpStatusCode.Created);
@@ -51,13 +58,28 @@ namespace AppointmentManagementAPI.Presentation.Controllers
             return CustomResult("Data found!", appointment, HttpStatusCode.NotFound);
         }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAppointment(int id, [FromBody] Appointment updatedAppointment)
+        [HttpPut]
+        public async Task<IActionResult> UpdateAppointment([FromBody] AppointmentDTO updatedAppointment)
         {
-            var isUpdated = await _service.Update(updatedAppointment);
+            if(!ModelState.IsValid)
+                return CustomResult("Invalid data!", HttpStatusCode.BadRequest);
+
+            var appointment = await _service.GetById(updatedAppointment.AppointmentId);
+            if (appointment == null)
+                return CustomResult("Data not found!", HttpStatusCode.NotFound);
+
+            var _appointment = new Appointment
+            {
+                PatientName = updatedAppointment.PatientName,
+                PatientContactInfo = updatedAppointment.PatientContactInfo,
+                AppointmentDateTime = updatedAppointment.AppointmentDateTime,
+                DoctorId = updatedAppointment.DoctorId
+            };
+            var isUpdated = await _service.Update(_appointment);
             if (!isUpdated)
                 return CustomResult("Data not updated!", HttpStatusCode.InternalServerError);
-            return CustomResult("Appointment updated successfully.", HttpStatusCode.OK);
+
+            return CustomResult("Appointment updated successfully.", updatedAppointment, HttpStatusCode.OK);
         }
 
         [HttpDelete("{id}")]
---
+++ AppointmentManagementAPI/UtilityModels/AppointmentDTO.cs
@@ -0,0 +1,20 @@
+﻿using AppoinmentManagementAPI.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentManagementAPI.Presentation.UtilityModels
+{
+    public record AppointmentDTO
+    (
+        int AppointmentId,
+
+        [Required(ErrorMessage = "Patient name is required")] string PatientName ,
+
+        [Required(ErrorMessage = "Patient email/phone number is required as info.")]
+         string PatientContactInfo,
+
+        [Required(ErrorMessage = "Appointment date & time is required")]
+        DateTime AppointmentDateTime ,
+
+        [Required(ErrorMessage = "Doctor is required"),Range(1,Int32.MaxValue)]
+         int DoctorId );
+}
